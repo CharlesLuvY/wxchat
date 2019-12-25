e3bd8be3c5e6d8f1b0f02e2a50b6402f494fb9b8
lixz <zgsddzlxz@outlook.com>
2019-12-26 03:04:41 +0800

    version1.4

--- chat.py
+++ chat.py
@@ -1,13 +1,10 @@
 # -*- coding:utf-8 -*-
 import web
+from handle import Handle
 
 urls = (
     '/wx', 'Handle',
 )
-
-class Handle(object):
-    def GET(self):
-        return "hello, this is handle view"
 
 if __name__ == '__main__':
     app = web.application(urls,globals())
---
+++ handle.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+import web
+
+
+class Handle(object):
+    def GET(self):
+
+        data = web.input()
+        if len(data) == 0:
+            return "hello, this is handle view"
+        signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        echostr = data.echostr
+        token = "zhanglaoguang"
+
+        l = [token, timestamp, nonce]
+        l.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
+        print("handle/GET func: hashcode, signature", hashcode, signature)
+        if hashcode == signature:
+            return echostr
+        else:
+            return"啥也没返回！"
+
+
