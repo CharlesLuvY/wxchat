8d533d1027985849788d0bc3313cf020169c09d2
lixz <zgsddzlxz@outlook.com>
2019-12-26 03:21:14 +0800

    fix web.py safety message

--- chat.py
+++ chat.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
 import web
 from handle import Handle
+from web.template import ALLOWED_AST_NODES
 
+ALLOWED_AST_NODES.append('Constant')
 urls = (
     '/wx', 'Handle',
 )
 
 if __name__ == '__main__':
-    app = web.application(urls,globals())
+    app = web.application(urls, globals())
     app.run()
